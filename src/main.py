742412673ea6ddf44cfac3eacb0bee21c5368a96
Xiang Gao <gxiang1228@gmail.com>
2019-04-14 13:45:48 +0800

    revert

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@
 from lib import *
 from sampler import *
 from agents import *
-from env import *
-from simulator import *
+from emulator import *
+from simulators import *
 from visualizer import *
 
 
